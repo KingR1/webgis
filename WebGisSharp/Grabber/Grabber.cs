a5d1c9645313e75e6baf4621a3b09fb443495519
Taras Boiko <kilotaras@gmail.com>
2013-12-18 21:26:02 +0200

    Created project, added model for memorial.

---
+++ WebGisSharp/Grabber/Grabber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grabber.Models;
+
+namespace Grabber
+{
+    public class Grabber
+    {
+        IEnumerable<Memorial> Grab()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebGisSharp/Grabber/Models/Memorial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grabber.Models
+{
+    public class Memorial
+    {
+        public string Name { get; set; }
+        // TODO: check for specific datatype for spatial geocoordinates 
+       public double X {get;set;}
+       public double Y { get; set; }
+    }
+}
